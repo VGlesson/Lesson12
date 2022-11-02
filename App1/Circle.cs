73adf04a856e6563b3073469cc028343dea36fd0
Владимир Гаврилов <saxar.1kg@mail.ru>
2022-11-02 12:21:49 +0300

    Добавьте файлы проекта.

---
+++ App1/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    static class Circle
+    {
+        static public double GetLength(int r = 0)
+        {
+            return 2 * Math.PI * r;
+        }
+        static public double GetSquare(int r = 0)
+        {
+            return Math.PI * r * r;
+        }
+        static public double GetCentrCircle(double x = 0, double y = 0, int r = 0, double a, double b);
+        {
+        return (x-a)*2(y-b)*2=r*2;
+        }
+    } 
+}
---
+++ App1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус");
+            int r = Convert.ToInt32(Console.ReadLine());
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите  точку по х");
+            double x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите  точку по х");
+            double y = Convert.ToInt32(Console.ReadLine());
+            double length = Circle.GetLength();
+            double square = Circle.GetSquare();
+            double center = Circle.GetCentrCircle();
+            Console.WriteLine("L = {Length} S = {Square} C={Ctnter}");
+            Console.ReadKey();
+
+        }
+    }
+}
